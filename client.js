69fccb916c9a563881d16c1d862c6c87ef5d2c24
sibansu1211 <98813942+sibansu1211@users.noreply.github.com>
2023-08-06 22:49:09 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-// server.js
+
 io.on('connection', (socket) => {
     socket.on('send-chat-message', (roomId, message) => {
         socket.to(roomId).broadcast.emit('chat-message', {
@@ -8,18 +8,17 @@ io.on('connection', (socket) => {
     });
 });
 
-// client.js
-// Emit chat messages
+
 const chatForm = document.getElementById('chat-form');
 chatForm.addEventListener('submit', (event) => {
     event.preventDefault();
     const message = chatForm.querySelector('input').value;
     socket.emit('send-chat-message', roomId, message);
-    // Clear input field
+    
     chatForm.querySelector('input').value = '';
 });
 
-// Display received chat messages
+
 socket.on('chat-message', (data) => {
     // Display the chat message in the UI
 });
